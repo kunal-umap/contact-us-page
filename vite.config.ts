677c6f9f3d728be07d7d71767a059f023fc890a4
Kunal Umap <kunalgumap@gmail.com>
2024-06-29 01:50:52 +0530

    Minor Deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/OnestopNDT/",
+  base: "/contact-us-page/",
   plugins: [react()],
 })
